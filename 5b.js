69598bfb3893f894bae631d0afb8572ae54e2291
greensofa24 <107178637+greensofa24@users.noreply.github.com>
2025-04-03 14:44:06 +1100

    Update 5b.js

--- 5b.js
+++ 5b.js
@@ -644,6 +644,8 @@ const blockProperties = [
 	[false,false,false,false,false,false,false,false,false,true,true,0,0,false,false,false,1,false],
 	[false,true,false,false,false,false,false,false,true,false,false,0,2,false,false,true,1,false],
 	[false,false,false,false,false,false,false,false,true,false,false,0,2,false,false,true,1,false],
+	// customtesting
+	[true,true,true,true,false,false,false,false,true,false,false,0,1,false,false,true,1,false],
 ];
 const switches = [[31,33,32,34,79,78,81,82],[51,53,52,54,133,134],[65,61,60,62,63,64],[],[],[14,16,83,85]];
 
