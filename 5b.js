434bf65b1c89cdd534ee3a1a4f57abf6222b117d
greensofa24 <107178637+greensofa24@users.noreply.github.com>
2025-12-09 09:19:14 +1100

    really stupid system

--- 5b.js
+++ 5b.js
@@ -642,8 +642,10 @@ const blockProperties = [
 	[false,true,false,false,false,false,false,false,true,false,false,0,2,false,false,true,1,false],
 	[false,false,false,false,false,false,false,false,true,false,false,0,2,false,false,true,1,false],
 	// custom
+	// let's start without animations
+	// default block
+	[true,true,true,true,false,false,false,false,false,false,false,0,0,true,false,true,1,false],
 	// [false,false,false,false,true,true,true,true,true,false,false,0,0,false,false,true,1,false],
-	// [true,true,true,true,false,false,false,false,true,false,false,0,0,false,false,true,14,false,[0,1,2,3,4,5,6,7,8,9,10,11,12,13]],
 ];
 const switches = [[31,33,32,34,79,78,81,82],[51,53,52,54,133,134],[65,61,60,62,63,64],[],[],[14,16,83,85]];
 
